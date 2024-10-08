be120cfd76328bbbf1d4842416a8f10f7479c7be
a.boikov <alex8oikov@yandex.ru>
2024-10-08 12:17:31 +0300

    добавил комментарий по названию

--- TregisterPerson/TregisterPerson.cs
+++ TregisterPerson/TregisterPerson.cs
@@ -7,6 +7,7 @@ namespace TregisterPerson
     public class TregisterPerson
     {
         [TestMethod]
+        /// REVIEW. a.boikov. 2024/10/08. Конструктор по англ. Construct, Konstrukt
         public void TestKonstrukt()
         {
             RegisteredPerson registerPerson = new RegisteredPerson("Иван", "Смирнов");
